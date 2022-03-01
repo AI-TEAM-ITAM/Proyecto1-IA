b5a0b8cd09857c9a43d35c00e99b182371459eb9
AccelR8 <55751739+Axelord100@users.noreply.github.com>
2022-02-28 21:06:18 -0600

    Add files via upload

--- evalua.pl
+++ evalua.pl
@@ -1,6 +1,16 @@
-evalua(Pol,X,Res):-
-    evalua2(Pol, X, 0, 0).
-evalua2([Cabeza|Cola], X, Exp, Res):-
-    Res is Res+Cabeza*X^Exp,
-    Exp is Exp+1,
-    evalua2(Cola, X, Exp, Res).
+%Evalúa una X en el polinomio dado un polinomio, un grado y su respuesta 
+
+evalua(Pol, X, Resp) :- 
+	evaluaPol(Pol, X, 0, 0, Resp). %Comienza con grado y contador en 0
+
+evaluaPol([Cabeza|Cola], X, Grado, Count, Resp) :-
+        RespN is (Count + Cabeza * (X**Grado)), %Actualiza respuesta; suma contador, coef. en la cabeza, eleva Xal respectivo grado 
+        GradoN is Grado + 1, %actualiza grado
+        evaluaPol(Cola, X, GradoN, RespN, Resp).  
+
+%Caso base cuando se tenga la lista vacía sin importar de los demás parámetros, se detiene
+evaluaPol([], _X, _Grado, Count, Count).
+
+/* evalua([1, 2, 3], 3, X).
+ * X = 34.
+ */
